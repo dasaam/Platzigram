36447f8a4f65a7dabb1c5056facddf0b9f34d7f1
DANIEL SANCHEZ <daniel@alohacreativos.com>
2020-02-01 08:57:22 -0600

    Tomando foto y guardando en el dispositivo

--- app/src/main/java/com/example/dakane/platzigram/view/fragments/NewPostFragment.java
+++ app/src/main/java/com/example/dakane/platzigram/view/fragments/NewPostFragment.java
@@ -1,19 +1,37 @@
 package com.example.dakane.platzigram.view.fragments;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.dakane.platzigram.R;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewPostFragment extends Fragment {
-
+    ImageView ivPicture;
+    Button btnPicture;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
 
     public NewPostFragment() {
         // Required empty public constructor
@@ -24,7 +42,78 @@ public class NewPostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_new_post, container, false);
+
+        ivPicture = (ImageView) view.findViewById(R.id.ivPicture);
+        btnPicture = (Button)view.findViewById(R.id.btnTakePicture);
+
+        btnPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
+        return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK){
+
+            /*Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ivPicture.setImageBitmap(imageBitmap);*/
+            Picasso.with(getActivity()).load(mCurrentPhotoPath).into(ivPicture);
+            addPictureToGallery();
+            Toast.makeText(getActivity(), mCurrentPhotoPath, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void takePicture(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null ){
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(getActivity(),
+                        "com.example.dakane.platzigram",
+                        photoFile
+                        );
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+
+        }
+    }
+
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPG"+timeStamp+"_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(
+            imageFileName,
+                ".jpg",
+                storageDir
+        );
+        mCurrentPhotoPath = "file:"+image.getAbsolutePath();
+        return image;
+    }
+
+    private void addPictureToGallery(){
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File newFile = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(newFile);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
+    }
+
+
 
 }
