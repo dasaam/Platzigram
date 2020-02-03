5b1e4f2bc1080de7d0e9e0f2bc99600ee8670f98
DANIEL SANCHEZ <daniel@alohacreativos.com>
2020-02-03 12:01:07 -0600

    Agregar firebase al proyecto y preparar el login con email

--- app/src/main/java/com/example/dakane/platzigram/LoginActivity.java
+++ app/src/main/java/com/example/dakane/platzigram/LoginActivity.java
@@ -1,29 +1,73 @@
 package com.example.dakane.platzigram;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.dakane.platzigram.view.ContainerActivity;
 import com.example.dakane.platzigram.view.CreateAccountActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        final TextInputEditText etEmail = (TextInputEditText)findViewById(R.id.username);
+        final TextInputEditText etPassword = (TextInputEditText)findViewById(R.id.password);
+
+        Button btnLogin = (Button)findViewById(R.id.login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                if (email.equals("")){
+                    Toast.makeText(LoginActivity.this, "Pon un email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (password.equals("")){
+                    Toast.makeText(LoginActivity.this, "Pon un password", Toast.LENGTH_LONG).show();
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()){
+                                    Toast.makeText(LoginActivity.this, "Fallo el login" + task.getException(), Toast.LENGTH_LONG).show();
+                                }else{
+                                    goClickMenu();
+                                }
+                            }
+                        });
+
+            }
+        });
+
     }
     public void goClickAccount(View view){
         Intent intent = new Intent(this, CreateAccountActivity.class);
         startActivity(intent);
     }
-    public void goClickMenu(View view){
+    public void goClickMenu(){
         Intent intent = new Intent(this, ContainerActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/dakane/platzigram/view/CreateAccountActivity.java
+++ app/src/main/java/com/example/dakane/platzigram/view/CreateAccountActivity.java
@@ -1,19 +1,76 @@
 package com.example.dakane.platzigram.view;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.dakane.platzigram.LoginActivity;
 import com.example.dakane.platzigram.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         showToolbar(getResources().getString(R.string.toolbar_tittle_createaccount), true);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        final TextInputEditText etEmail = (TextInputEditText)findViewById(R.id.email);
+        final TextInputEditText etName = (TextInputEditText)findViewById(R.id.name);
+        final TextInputEditText etUser = (TextInputEditText)findViewById(R.id.user);
+        final TextInputEditText etPassword = (TextInputEditText)findViewById(R.id.password_createaccount);
+        final TextInputEditText etConfirmPass = (TextInputEditText)findViewById(R.id.confirmPassword);
+
+        Button btnCreateAccount = (Button)findViewById(R.id.joinUs);
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString().trim();
+                String user = etUser.getText().toString().trim();
+                String userName = etName.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                String confirmPass = etConfirmPass.getText().toString().trim();
+
+                if (email.equals("")){
+                    Toast.makeText(getApplicationContext(), "Pon un email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (!password.equals(confirmPass)){
+                    if (password.equals("")){
+                        Toast.makeText(getApplicationContext(), "Pon un password", Toast.LENGTH_LONG).show();
+                    }
+                    return;
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(CreateAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()){
+                                    Toast.makeText(getApplicationContext(), "Autenticacion fallo" + task.getException(), Toast.LENGTH_LONG).show();
+                                }else{
+                                    startActivity(new Intent(CreateAccountActivity.this, LoginActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
     }
 
     public void showToolbar(String tittle, boolean upButton){
